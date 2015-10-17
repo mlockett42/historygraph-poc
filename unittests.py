188791bd5b431a0635dda4e9650e037245c6a5d7
Mark Lockett <mark@deliveryengine.net>
2015-10-17 08:38:42 +0800

    Add the doop classes from the c++ propject - this does not actually work yet

---
+++ unittests.py
@@ -0,0 +1,189 @@
+class Covers(Document):
+    covers = FieldInt()
+
+def testSimpleCovers():
+    #Test merging together simple covers documents
+    test = Covers()
+    test.covers = 1
+    #Test we can set a value
+    assert test.covers == 1
+    test2 = Covers(test.id)
+    test.GetHistory().Replay(test2)
+    #Test we can rebuild a simple object by playing an edge
+    assert test2.covers == 1
+    #Test these are just the same history object but it was actually copied
+    assert test.GetHistory() is not test2.GetHistory()
+    
+    test3 = test2.Clone()
+    #Test the clone is the same as the original. But not just refering to the same object
+    assert test3.covers == test2.covers
+    assert test2 is not test3
+    assert test2.GetHistory() is not test3.GetHistory()
+    
+    test = Covers()
+    test.covers = 1
+    test.covers = 2
+    test2 = Covers(test.id)
+    test.GetHistory().Replay(test2)
+    assert test.covers == 3
+    assert test.GetHistory() is not test2.GetHistory()
+    assert test is not test2
+    
+def testMergeHistoryCover():
+    #Test merge together two simple covers objects
+    test = Covers()
+    test.covers = 1
+    test2 = test.Clone()
+    test.covers = 2
+    test2.covers = 3
+    test3 = test.Merge(test2)
+    #In a merge conflict between two integers to great one is 
+    assert test3.covers == 3
+
+class TestPropertyOwner2(DocumentObject):
+    cover = FieldInt()
+    quantity = FieldInt()
+
+class TestPropertyOwner1(Document):
+    covers = FieldInt()
+    propertyowner2s = FieldList(TestPropertyOwner2)
+    def WasChanged(changetype, propertyowner, propertyname, propertyvalue, propertytype):
+        super(TestPropertyOwner1, self).WasChanged(changetype, propertyowner, propertyname, propertyvalue, propertytype)
+        self.bWasChanged = True
+
+def testListItemChangeHistory():
+    #Test that various types of changes create was changed events
+    test1 = TestPropertyOwner1()
+    test1.bWasChanged = False
+    test2 = TestPropertyOwner2()
+    test1.propertyowner2s.add(test2)
+    assert test1.bWasChanged = True
+    test1.bWasChanged = False
+    test2.cover = 1
+    assert test1.bWasChanged = True
+    test1.bWasChanged = False
+    test2.cover = 1
+    assert test1.bWasChanged = True
+    test1.propertyowner2s.remove(test2)
+
+def testSimpleItem():
+    test1 = TestPropertyOwner1()
+    testitem = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem)
+    testitem.cover = 1
+    #Test semantics for retriving objects
+    assert len(test1.propertyowner2s) == 1
+    for po2 in test1.propertyowner2s
+        assert po2.__class__.__name__ == TestPropertyOwner2.__name__
+        assert po2.cover == 1
+
+    test1 = TestPropertyOwner1()
+    testitem = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem)
+    testitem.cover = 1
+    test1.propertyowner2s.remove(testitem)
+
+    test2 = TestPropertyOwner1()
+    test1.GetHistory().Replay(test2)
+
+    #Check that replaying correctly removes the object
+    assert len(test2.propertyowner2s) == 0
+
+    test1 = TestPropertyOwner1()
+    testitem = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem)
+    testitem.cover = 1
+    test2 = test1.Clone()
+    
+    assert len(test2.propertyowner2s) == 1
+    for po2 in test2.propertyowner2s
+        assert po2.__class__.__name__ == TestPropertyOwner2.__name__
+        assert po2.cover == 1
+
+def testAdvancedItem():
+    #Test changing them deleting a sub element
+    test1 = TestPropertyOwner1()
+    testitem1 = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem1)
+    testitem1.cover = 1
+    test2 = test1.Clone()
+    testitem2 = test2.GetDocumentObject(testitem1.id)
+    testitem2.cover = 2
+    test2.propertyowner2s.remove(testitem2)
+    test3 = test1.Merge(test2)
+    assert len(test3.propertyowner2s) == 0
+
+    #Test changing them deleting a sub element. Test merging in the opposition order to previous test
+    test1 = TestPropertyOwner1()
+    testitem1 = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem1)
+    testitem1.cover = 1
+    test2 = test1.Clone()
+    testitem2 = test2.GetDocumentObject(testitem1.id)
+    testitem2.cover = 2
+    test2.propertyowner2s.remove(testitem2)
+    test3 = test2.Merge(test1)
+    assert len(test3.propertyowner2s) == 0
+
+    #Test merging changes to different objects in the same document works
+    test1 = TestPropertyOwner1()
+    testitem1 = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem1)
+    test2 = test1.Clone()
+    testitem1.cover = 3
+    test2.covers=2
+    test3 = test2.Merge(test1)
+    assert len(test1.propertyowner2s) == 1
+    for item1 in test1.propertyowner2s:
+        assert item1.cover == 3
+    assert test3.covers == 2
+
+    #Test changing different objects on different branches works
+    test1 = TestPropertyOwner1()
+    testitem1 = TestPropertyOwner2()
+    id1 = testitem1.id
+    test1.propertyowner2s.add(testitem1)
+    testitem2 = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem2)
+    id2 = testitem2.id
+    test2 = test1.Clone()
+    testitem2 = test2.GetDocumentObject(id2)
+    testitem1.cover = 2
+    testitem1.quantity = 3
+    testitem2.cover = 3
+    testitem2.quantity = 2
+    test3 = test2.Merge(test1)
+    testitem1 = test3.GetDocumentObject(id1)
+    testitem2 = test3.GetDocumentObject(id2)
+    assert testitem2.cover == 3
+    assert testitem2.quantity == 2
+    assert testitem1.cover == 2
+    assert testitem1.quantity == 3
+    
+    #Test changing different objects on different branches works reverse merge of above
+    test1 = TestPropertyOwner1()
+    testitem1 = TestPropertyOwner2()
+    id1 = testitem1.id
+    test1.propertyowner2s.add(testitem1)
+    testitem2 = TestPropertyOwner2()
+    test1.propertyowner2s.add(testitem2)
+    id2 = testitem2.id
+    test2 = test1.Clone()
+    testitem2 = test2.GetDocumentObject(id2)
+    testitem1.cover = 2
+    testitem1.quantity = 3
+    testitem2.cover = 3
+    testitem2.quantity = 2
+    test3 = test1.Merge(test2)
+    testitem1 = test3.GetDocumentObject(id1)
+    testitem2 = test3.GetDocumentObject(id2)
+    assert testitem2.cover == 3
+    assert testitem2.quantity == 2
+    assert testitem1.cover == 2
+    assert testitem1.quantity == 3
+    
+    
+    
+    
+
+    
