4488ef3a0ebc43958771f2e6a6dd6fcc3b7df575
Mark Lockett <mark@deliveryengine.net>
2016-06-06 00:00:30 +0800

    Add the ability to do simple edge adds without a full replay

--- unittests.py
+++ unittests.py
@@ -35,6 +35,8 @@ import time
 from json import JSONEncoder, JSONDecoder
 from Crypto.Hash import SHA256
 import DocumentCollectionHelper
+import hashlib
+from HistoryEdgeNull import HistoryEdgeNull
 
 class Covers(Document):
     def __init__(self, id):
@@ -120,6 +122,59 @@ class MergeHistorySendEdgeCoverTestCase(unittest.TestCase):
         history.Replay(test3)
         #In a merge conflict between two integers the greater one is the winner
         self.assertEqual(test3.covers, 3)
+
+        #Test the behaviour of receiving edges out of order
+        test = Covers(None)
+        test.covers = 1
+        test2 = test.Clone()
+        test.covers = 2
+        test5 = test2.Clone()
+        test6 = test2.Clone()
+        test2.covers = 3
+        test2.covers = 4
+        edge4 = test2.history.edgesbyendnode[test2.currentnode]
+        edge3 = test2.history.edgesbyendnode[list(edge4.startnodes)[0]]
+        history = test.history.Clone()
+        history.AddEdge(edge4)
+        test3 = Covers(test.id)
+        history.Replay(test3)
+        #edge4 should be orphaned and not played
+        self.assertEqual(test3.covers, 2)
+        #Once edge3 is added we should replay automatically to 4
+        history.AddEdge(edge3)
+        test4 = Covers(test.id)
+        history.Replay(test4)
+        #edge4 should be orphaned and not played
+        self.assertEqual(test4.covers, 4)
+
+        #Test live adding of edges
+        test5.AddEdge(edge3)
+        self.assertEqual(test5.covers, 3)
+        
+        #Test live adding of orphaned edges
+        test6.AddEdge(edge4)
+        self.assertEqual(test6.covers, 1)
+        test6.AddEdge(edge3)
+        self.assertEqual(test6.covers, 4)
+
+        #Test adding a Null edge where we don't  have one of the start nodes.
+        #In the old way        
+        dummysha = hashlib.sha256('Invalid node').hexdigest()
+        history = test.history.Clone()
+        edgenull = HistoryEdgeNull({test6.currentnode, dummysha}, "", "", "", "", test6.id, test6.__class__.__name__)
+        history.AddEdge(edgenull)
+        test6 = Covers(test.id)
+        history.Replay(test6)
+        self.assertEqual(test6.covers, 2)
+
+        #In the new way
+        test6 = test.Clone()
+        oldnode = test6.currentnode
+        edgenull = HistoryEdgeNull({test6.currentnode, dummysha}, "", "", "", "", test6.id, test6.__class__.__name__)
+        test6.AddEdge(edgenull)
+        self.assertEqual(test6.covers, 2)
+        self.assertEqual(test6.currentnode, oldnode)
+        
 
 class ListItemChangeHistoryTestCase(unittest.TestCase):
     def setUp(self):
