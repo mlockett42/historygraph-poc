e75017991aff9a76f2c1141df61ab48a99a1472c
Mark Lockett <mark@deliveryengine.net>
2016-09-05 19:01:03 +0800

    Remove old code no longer part of any tests

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -68,7 +68,6 @@ def I_choose_Settings_from_the_Options_menu(step):
     menu_item_name = "SETTINGS"
     menu_found = None
     for menu in world.formmain.menubar.children():
-        #utils.log_output("menu = ",menu)
         if type(menu) == QMenu and (menu.title().upper() == menu_name or menu.title().upper() == '&' + menu_name):
             menu_found = menu
     assert menu_found is not None
@@ -86,90 +85,4 @@ def I_choose_Settings_from_the_Options_menu(step):
         menu_item_found.trigger()
         world.formsettings = world.formmain.formsettings
 
-@step(u'Go to the settings page')
-def go_to_the_settings_page(step):
-    demux = Demux(fromfile = '/tmp/testdump.db')
-    form = FormSettings(demux)
-    QtTest.QTest.keyClicks(form.teEmailAddress, "mlockett1@livewire.io", 0, 10)
-    QtTest.QTest.keyClicks(form.tePOPServerName, "localhost1", 0, 10)
-    QtTest.QTest.keyClicks(form.tePOPServerPort, "10026", 0, 10)
-    QtTest.QTest.keyClicks(form.tePOPUserName, "mlockett1+1@livewire.io", 0, 10)
-    QtTest.QTest.keyClicks(form.tePOPPassword, "password1", 0, 10)
-    QtTest.QTest.keyClicks(form.teSMTPServerName, "localhost2", 0, 10)
-    QtTest.QTest.keyClicks(form.teSMTPServerPort, "10025", 0, 10)
-    QtTest.QTest.keyClicks(form.teSMTPUserName, "mlockett1+2@livewire.io", 0, 10)
-    QtTest.QTest.keyClicks(form.teSMTPPassword, "password2", 0, 10)
 
-    QtTest.QTest.mouseClick(form.bnOK, Qt.LeftButton)
-    
-    assert demux.myemail == "mlockett1@livewire.io"
-    assert demux.popserver == "localhost1"
-    assert demux.popport == "10026"
-    assert demux.popuser == "mlockett1+1@livewire.io"
-    assert demux.poppass == "password1"
-    assert demux.smtpserver == "localhost2"
-    assert demux.smtpuser == "mlockett1+2@livewire.io"
-    assert demux.smtppass == "password2"
-    assert demux.smtpport == "10025"
-
-    assert demux.settingsstore.LoadSetting('myemail') == "mlockett1@livewire.io"
-    assert demux.settingsstore.LoadSetting('smtpserver') == "localhost2"
-    assert demux.settingsstore.LoadSettingInt('smtpport') == 10025
-    assert demux.settingsstore.LoadSetting('smtpuser') == "mlockett1+2@livewire.io"
-    assert demux.settingsstore.LoadSetting('smtppass') == "password2"
-    assert demux.settingsstore.LoadSetting('popserver') == "localhost1"
-    assert demux.settingsstore.LoadSetting('popuser') == "mlockett1+1@livewire.io"
-    assert demux.settingsstore.LoadSetting('poppass') == "password1"
-    assert demux.settingsstore.LoadSettingInt('popport') == 10026
-
-    #Get a new demux from the database and test everything is loaded correctly
-    demux = Demux(fromfile = '/tmp/testdump.db')
-    form = FormSettings(demux)
-
-    assert demux.myemail == "mlockett1@livewire.io"
-    assert demux.popserver == "localhost1"
-    assert demux.popport == 10026
-    assert demux.popuser == "mlockett1+1@livewire.io"
-    assert demux.poppass == "password1"
-    assert demux.smtpserver == "localhost2"
-    assert demux.smtpuser == "mlockett1+2@livewire.io"
-    assert demux.smtppass == "password2"
-    assert demux.smtpport == 10025
-
-    assert demux.settingsstore.LoadSetting('myemail') == "mlockett1@livewire.io"
-    assert demux.settingsstore.LoadSetting('smtpserver') == "localhost2"
-    assert demux.settingsstore.LoadSettingInt('smtpport') == 10025
-    assert demux.settingsstore.LoadSetting('smtpuser') == "mlockett1+2@livewire.io"
-    assert demux.settingsstore.LoadSetting('smtppass') == "password2"
-    assert demux.settingsstore.LoadSetting('popserver') == "localhost1"
-    assert demux.settingsstore.LoadSetting('popuser') == "mlockett1+1@livewire.io"
-    assert demux.settingsstore.LoadSetting('poppass') == "password1"
-    assert demux.settingsstore.LoadSettingInt('popport') == 10026
-
-    assert demux.myemail == form.teEmailAddress.toPlainText()
-    assert demux.popport == int(form.tePOPServerPort.toPlainText())
-    assert demux.popserver == form.tePOPServerName.toPlainText()
-    assert demux.popuser == form.tePOPUserName.toPlainText()
-    assert demux.poppass == form.tePOPPassword.toPlainText()
-    assert demux.smtpserver == form.teSMTPServerName.toPlainText()
-    assert demux.smtpport == int(form.teSMTPServerPort.toPlainText())
-    assert demux.smtpuser == form.teSMTPUserName.toPlainText()
-    assert demux.smtppass == form.teSMTPPassword.toPlainText()
-    #assert False, 'This step must be implemented'
-"""
-@step(u'Fill in the field with the name "([^"]*)" with "([^"]*)"')
-def fill_in_the_field_with_the_name_group1_with_group2(step, group1, group2):
-    assert False, 'This step must be implemented'
-@step(u'Fill in the field with the name "([^"]*)" with ""')
-def fill_in_the_field_with_the_name_group1_with(step, group1):
-    assert False, 'This step must be implemented'
-@step(u'Then I see that the settings contain:')
-def then_i_see_that_the_settings_contain(step):
-    assert False, 'This step must be implemented'
-@step(u'email_address       | pop_server | pop_port | pop_username | pop_password | smtp_server | smtp_port | smtp_username | smtp_password |')
-def email_address_pop_server_pop_port_pop_username_pop_password_smtp_server_smtp_port_smtp_username_smtp_password(step):
-    assert False, 'This step must be implemented'
-@step(u'mlockett1@timeca.io | localhost  | 10025    | mlockett1    |              | localhost   | 10026     | mlockett1     |               |')
-def mlockett1_timeca_io_localhost_10025_mlockett1_localhost_10026_mlockett1(step):
-    assert False, 'This step must be implemented'
-"""
